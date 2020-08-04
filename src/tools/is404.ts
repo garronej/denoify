48bec86006e0cf4d0c13408c5b2590d9e63aeb71
Joseph Garrone <joseph.garrone.gj@gmail.com>
2020-08-05 01:20:17 +0200

    add jsdoc (changelog ignore)

--- src/tools/is404.ts
+++ src/tools/is404.ts
@@ -1,6 +1,7 @@
 
 import fetch from "node-fetch";
 
+/** Return true if 404 or 400 */
 export function is404(url: string): Promise<boolean> {
     return fetch(url).then(({ status }) => (status === 404 || status === 400));
 }

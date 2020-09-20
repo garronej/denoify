3b4e20b95b2b7af4bc00ca302ff69797733d1f52
garronej <joseph@semasim.com>
2020-09-20 17:07:19 +0200

    Cleanup (changelog ignore)

--- src/lib/getThirdPartyDenoModuleInfos.ts
+++ src/lib/getThirdPartyDenoModuleInfos.ts
@@ -6,66 +6,6 @@ import { addCache } from "../tools/addCache";
 // https://cdn.deno.land/evt/meta/versions.json
 // https://cdn.deno.land/evt/versions/v1.6.8/meta/meta.json
 
-/*
-import * as path from "path";
-import { getProjectRoot } from "../tools/getProjectRoot";
-
-const limit= 100;
-
-const listThirdPartyDenoModulesByPage = addCache(async (params: { page: number; }) => {
-
-    const { page } = params;
-
-    type Data = {
-        total_count: number;
-        results: { name: string; }[];
-    };
-
-    const data = await fetch(`https://api.deno.land/modules?limit=${limit}&page=${page}`)
-        .then(async res => JSON.parse(await res.text())["data"] as Data)
-
-    const { total_count, results } = data;
-
-    return {
-        total_count,
-        "names": results.map(({ name }) => name)
-    };
-
-}, {
-    "filePathForPersistanceAcrossRun": path.join(
-        getProjectRoot(), "res", "cache", "listThirdPartyDenoModulesByPage.json"
-    )
-});
-
-export async function* thirdPartyDenoModuleNames(): AsyncGenerator<string> {
-
-    let total_count: undefined | number = undefined;
-
-    const getLastPage = (params: { total_count: number; }) => Math.floor(params.total_count / limit) + 1;
-
-    let page = 1;
-
-    while (total_count === undefined || page <= getLastPage({ total_count })) {
-
-        const result = await listThirdPartyDenoModulesByPage({ page });
-
-        if (total_count === undefined) {
-            total_count = result.total_count;
-        }
-
-        for (const name of result.names) {
-            yield name;
-        }
-
-        page++;
-
-    }
-
-    return undefined;
-
-}
-*/
-
 export const getThirdPartyDenoModuleInfos = addCache(
     async (params: { denoModuleName: string; }): Promise<{
         owner: string;
@@ -113,14 +53,7 @@ export const getThirdPartyDenoModuleInfos = addCache(
             "subdir": infos.subdir ?? "/"
         };
 
-    },
-    /*
-    {
-        "filePathForPersistanceAcrossRun": path.join(
-            getProjectRoot(), "res", "cache", "getThirdPartyDenoModuleInfos.json"
-        )
     }
-    */
 );
 
 

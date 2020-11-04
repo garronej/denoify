51ea236df6a306bbdd562a1144df5b5d2df6ab37
ts_ci <ts_ci@github.com>
2020-11-04 03:17:36 +0000

    Enabling shorter import paths [automatic]

---
+++ bin/replacer/index.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const lib_1 = require("../../lib");
+lib_1.makeThisModuleAnExecutableReplacer(async (params) => {
+    for (const { replacer } of await Promise.all([
+        Promise.resolve().then(() => require("./fast-xml-parser")),
+        Promise.resolve().then(() => require("./graphql")),
+        Promise.resolve().then(() => require("./ipaddr.js")),
+        Promise.resolve().then(() => require("./react")),
+        Promise.resolve().then(() => require("./react-dom")),
+        Promise.resolve().then(() => require("./rxjs"))
+    ])) {
+        const output = await replacer(params);
+        if (output !== undefined) {
+            return output;
+        }
+    }
+    return undefined;
+});
+//# sourceMappingURL=index.js.map

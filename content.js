e0b806dcc98d16586d24f4997cf38fb339ff56fd
Joseph Garrone <joseph.garrone.gj@gmail.com>
2020-11-22 06:10:56 +0100

    update

--- content.js
+++ content.js
@@ -4,30 +4,29 @@ module.exports = {
   description: "🦕For NPM module authors that would like to support Deno but do not want to write and maintain a port for it.",
   preview: "https://denoify.land/assets/img/social_preview.png", //Social media preview image
   baseUrl: "https://denoify.land",
-  apiUrl: "https://docs.evt.land/api",
-  learnMoreUrl: "https://docs.evt.land",
+  apiUrl: "https://docs.denoify.land",
+  learnMoreUrl: "https://docs.denoify.land",
   githubUrl: "https://github.com/garronej/denoify",
-  stackblitzUrl: "https://stackblitz.com/edit/evt-playground?embed=1&file=index.ts&hideExplorer=1",
   "quotes": [
     {
       "logo": "assets/img/logo_evt.png",
       "url": "https://evt.land",
       "quote": `<a target="_blank" rel="noopener noreferrer" href="https://evt.land">EVT</a>, 
-      is a type safe replacement for node's <code>EventEmitter</code>.`
+      is a type safe replacement for node's EventEmitter.`
     },
     {
       "logo": "assets/img/logo_nest.png",
       "url": "https://nest.land",
-      "quote": `<a target="_blank" rel="noopener noreferrer" href="https://github.com/nestdotland/yolk/tree/master/src/common">Yolk</a>, 
-      nest.land's official client library.<br>
-      nest.land is a module registry for Deno, on the blockweave. It enable to publish Deno modules to the permaweb, where they can never be deleted.
+      "quote": `Yolk, 
+      Nest.land's official client library.<br>
+      <a target="_blank" rel="noopener noreferrer" href="https://nest.land">Nest.land</a> is a module registry for Deno, on the blockweave. It enable to publish Deno modules to the permaweb, where they can never be deleted.
       `
     },
     {
       "logo": "assets/img/logo_eta.png",
-      "url": "https://eta.js.land",
-      "quote": `<a target="_blank" rel="noopener noreferrer" href="https://eta.js.land">Eta</a>, 
-      Is a template engine used under the hood by the widely popular Facebook's 
+      "url": "https://eta.js.org",
+      "quote": `<a target="_blank" rel="noopener noreferrer" href="https://eta.js.org">Eta</a>, 
+      Is the template engine used under the hood by the widely popular Facebook's 
       <a target="_blank" rel="noopener noreferrer" href="https://v2.docusaurus.io">Docusaurus</a>`
     },
     {

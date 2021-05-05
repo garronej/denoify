f0fa3c318963c94c115aa280b0215d5a53edbe1a
garronej <joseph.garrone.gj@gmail.com>
2021-05-05 12:12:01 +0200

    Improve wording on command line arguments

--- src/bin/denoify.ts
+++ src/bin/denoify.ts
@@ -10,13 +10,13 @@ commanderStatic
     For NPM module authors that would like to support Deno but do not want to write and maintain a port.
     See https://github.com/garronej/denoify for instructions.
     `)
-    .option("-p, --project [projectPath]", `Default: './' -- Denoify the project given to a folder with a 'package.json' and 'tsconfig.json'.`)
+    .option("-p, --project [projectPath]", `Default: './' -- Root path of the project to denoify, target directory is supposed to contain a 'package.json' and 'tsconfig.json'.`)
     .option("--src [srcDirPath]", `Default: '[projectPath]/src' | '[projectPath]/lib' -- Path to the directory containing the source .ts files. If the provided path is not absolute it is assumed relative to [projectPath]`)
     ;
 
 commanderStatic.parse(process.argv);
 
-const options = commanderStatic.opts()
+const options = commanderStatic.opts();
 
 denoify({
     "projectPath": options.project,

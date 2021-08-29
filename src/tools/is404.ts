0a800dff819b20cb9ddee02055ad8cee3fac618e
garronej <joseph.garrone.gj@gmail.com>
2021-08-29 16:13:03 +0200

    If the server dosen't respond, assume 404

--- src/tools/is404.ts
+++ src/tools/is404.ts
@@ -2,6 +2,16 @@
 import fetch from "node-fetch";
 
 /** Return true if 404 or 400 */
-export function is404(url: string): Promise<boolean> {
-    return fetch(url).then(({ status }) => (status === 404 || status === 400));
+export async function is404(url: string): Promise<boolean> {
+
+    try {
+
+        return await fetch(url, { "timeout": 2000 })
+            .then(({ status }) => (status === 404 || status === 400));
+
+    } catch {
+
+        return true;
+
+    }
 }

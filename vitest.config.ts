332b77c36cc70b4ef9a1a2f607f4c71ce3f5baf0
GervinFung <gervinfungdaxuen@gmail.com>
2022-10-07 00:45:31 +0800

    test(setup): installed & setup vitest

---
+++ vitest.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+    test: {
+        // ref: https://vitest.dev/config/
+        globals: true,
+        include: ["src/tests/index.ts"]
+    }
+});

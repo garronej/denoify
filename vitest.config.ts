9966a55d648dc393233ca822b66fff239f3ba62d
GervinFung <gervinfungdaxuen@gmail.com>
2022-10-08 23:08:02 +0800

    test(vite-config): removed enable global typing config

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
     "test": {
         // ref: https://vitest.dev/config/
-        "globals": true,
         "include": ["test/index.ts"],
         "watch": false,
         "outputFile": "./vitest-report.json",

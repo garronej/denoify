2b99a5d98591a7ed7506d1dddaef538a35e7c05a
garronej <joseph.garrone.gj@gmail.com>
2022-10-23 14:36:12 +0200

    Only support .denoifyrc.json and .denoifyrc.js

--- test/config/index.ts
+++ test/config/index.ts
@@ -1,6 +1,7 @@
 import { describe, it, expect } from "vitest";
 import getFileTypeAndContent from "../../src/lib/config/fileAndContent";
 import { parseAsDenoifyConfig } from "../../src/lib/config/parseParams";
+import { assert } from "tsafe/assert";
 
 const testParseParams = () =>
     describe("parse denoify config params", () => {
@@ -19,7 +20,7 @@ const testParseParams = () =>
             ).toBeUndefined();
         });
 
-        it.each([[".denoifyrc", ".denoifyrc.json", ".denoifyrc.yaml", , ".denoifyrc.yml"]])(
+        it.each([[".denoifyrc.json", ".denoifyrc.yaml"]])(
             "should parse each YAML/JSON config from '%s' and return the value of each key-value pairs",
             async configFileBasename => {
                 const configFileRawContent = `{${[`"out": "${configDummy.out}"`, `"index": "${configDummy.index}"`].join(",")}}`;
@@ -43,38 +44,37 @@ const testParseParams = () =>
                 });
 
                 expect(yaml).toBeTruthy();
+                assert(yaml !== undefined);
                 expect(yaml.out).toBe(configDummy.out);
                 expect(yaml.index).toBe(configDummy.index);
             }
         );
 
-        it.each([[".denoifyrc.js", "denoify.config.js", "denoify.config.cjs"]])(
-            "should parse each JavaScript config and return the value of each key-value pairs",
-            async configFileBasename => {
-                const configFileRawContent = `module.exports = {${[`out: "${configDummy.out}"`, `index: "${configDummy.index}"`].join(",")}}`;
-                const configFileType = await getFileTypeAndContent({
-                    "getConfigFileRawContent": () => Promise.resolve(configFileRawContent)
-                });
+        it.each([[".denoifyrc.js"]])("should parse each JavaScript config and return the value of each key-value pairs", async configFileBasename => {
+            const configFileRawContent = `module.exports = {${[`out: "${configDummy.out}"`, `index: "${configDummy.index}"`].join(",")}}`;
+            const configFileType = await getFileTypeAndContent({
+                "getConfigFileRawContent": () => Promise.resolve(configFileRawContent)
+            });
 
-                expect(configFileType.type).toBe("js");
+            expect(configFileType.type).toBe("js");
 
-                switch (configFileType.type) {
-                    case "yaml":
-                    case "absent":
-                        throw new Error("The only valid type is js and its asserted to be so");
-                }
-
-                expect(configFileType.configFileBasename).toBe(configFileBasename);
-                expect(configFileType.configFileRawContent).toBe(configFileRawContent);
-
-                const moduleExports = parseAsDenoifyConfig({
-                    configFileType
-                });
-
-                expect(moduleExports.out).toBe(configDummy.out);
-                expect(moduleExports.index).toBe(configDummy.index);
+            switch (configFileType.type) {
+                case "yaml":
+                case "absent":
+                    throw new Error("The only valid type is js and its asserted to be so");
             }
-        );
+
+            expect(configFileType.configFileBasename).toBe(configFileBasename);
+            expect(configFileType.configFileRawContent).toBe(configFileRawContent);
+
+            const moduleExports = parseAsDenoifyConfig({
+                configFileType
+            });
+            assert(moduleExports !== undefined);
+
+            expect(moduleExports.out).toBe(configDummy.out);
+            expect(moduleExports.index).toBe(configDummy.index);
+        });
     });
 
 export default testParseParams;

1773ff8e5eb0bb56db623da20909780e34d4fbf8
GervinFung <gervinfungdaxuen@gmail.com>
2023-01-17 19:39:15 +0800

    test(*): allow tests to be ran in parallel

---
+++ test/resolveNodeModuleToDenoModule/getValidImportUrl/test1.test.ts
@@ -0,0 +1,93 @@
+import { describe, it, expect } from "vitest";
+import { getValidImportUrlFactory } from "../../../src/lib/resolveNodeModuleToDenoModule";
+import { ModuleAddress } from "../../../src/lib/types/ModuleAddress";
+import { getLatestTag } from "../../../src/tools/githubTags";
+import { parseGetValidImportUrlResultAsCouldConnect } from "./shared";
+
+describe("test 1", () => {
+    it.each(["", "github: "])("should get valid url for ts-md5 with prefix: '%s'", prefix => {
+        const moduleAddress: ModuleAddress.GitHubRepo = {
+            "type": "GITHUB REPO",
+            "userOrOrg": "garronej",
+            "repositoryName": "ts-md5",
+            "branch": undefined
+        };
+
+        expect(ModuleAddress.parse(`${prefix}garronej/ts-md5`)).toStrictEqual(moduleAddress);
+        expect(ModuleAddress.parse(`${prefix}garronej/ts-md5#1.2.7`)).toStrictEqual({
+            ...moduleAddress,
+            "branch": "1.2.7"
+        });
+    });
+
+    it.each(["./dist/parallel_hasher", "dist/parallel_hasher", "./parallel_hasher", "parallel_hasher"])(
+        "should fallback to available latest version and get the valid url file path of '%s' for ts-md5 when the latest version specified is not available",
+        async specificImportPath => {
+            const moduleAddress: ModuleAddress.GitHubRepo = {
+                "type": "GITHUB REPO",
+                "userOrOrg": "garronej",
+                "repositoryName": "ts-md5",
+                "branch": undefined
+            };
+
+            const getValidImportUrlFactoryResult = await getValidImportUrlFactory({
+                "moduleAddress": moduleAddress,
+                "desc": "MATCH VERSION INSTALLED IN NODE_MODULES",
+                "version": "99.99.99"
+            });
+
+            expect(getValidImportUrlFactoryResult.couldConnect).toBe(true);
+
+            const { versionFallbackWarning, getValidImportUrl } = parseGetValidImportUrlResultAsCouldConnect(getValidImportUrlFactoryResult);
+
+            expect(typeof versionFallbackWarning).toBe("string");
+
+            const latestTag = await getLatestTag({
+                "owner": moduleAddress.userOrOrg,
+                "repo": moduleAddress.repositoryName
+            });
+
+            expect(typeof latestTag).toBe("string");
+
+            expect(await getValidImportUrl({ "target": "DEFAULT EXPORT" })).toBe(
+                `https://raw.githubusercontent.com/garronej/ts-md5/${latestTag}/deno_dist/mod.ts`
+            );
+            expect(await getValidImportUrl({ "target": "SPECIFIC FILE", "specificImportPath": "dist/parallel_hasher" })).toBe(
+                `https://raw.githubusercontent.com/garronej/ts-md5/${latestTag}/deno_dist/parallel_hasher.ts`
+            );
+
+            expect(await getValidImportUrl({ "target": "SPECIFIC FILE", specificImportPath })).toBe(
+                `https://raw.githubusercontent.com/garronej/ts-md5/${latestTag}/deno_dist/parallel_hasher.ts`
+            );
+        }
+    );
+
+    it("should get the latest version and its valid url file path for ts-md5 when the latest version specified is available", async () => {
+        const moduleAddress: ModuleAddress.GitHubRepo = {
+            "type": "GITHUB REPO",
+            "userOrOrg": "garronej",
+            "repositoryName": "ts-md5",
+            "branch": undefined
+        };
+
+        const getValidImportUrlFactoryResult = await getValidImportUrlFactory({
+            "moduleAddress": moduleAddress,
+            "desc": "MATCH VERSION INSTALLED IN NODE_MODULES",
+            "version": "1.2.7"
+        });
+
+        expect(getValidImportUrlFactoryResult.couldConnect).toBe(true);
+
+        const { versionFallbackWarning, getValidImportUrl } = parseGetValidImportUrlResultAsCouldConnect(getValidImportUrlFactoryResult);
+
+        expect(versionFallbackWarning).toBeUndefined();
+
+        expect(await getValidImportUrl({ "target": "DEFAULT EXPORT" })).toBe(
+            "https://raw.githubusercontent.com/garronej/ts-md5/v1.2.7/deno_dist/mod.ts"
+        );
+
+        expect(await getValidImportUrl({ "target": "SPECIFIC FILE", "specificImportPath": "dist/parallel_hasher" })).toBe(
+            "https://raw.githubusercontent.com/garronej/ts-md5/v1.2.7/deno_dist/parallel_hasher.ts"
+        );
+    });
+});
---
+++ test/resolveNodeModuleToDenoModule/getValidImportUrl/test4.test.ts
@@ -0,0 +1,84 @@
+import { describe, it, expect } from "vitest";
+import { getValidImportUrlFactory } from "../../../src/lib/resolveNodeModuleToDenoModule";
+import { ModuleAddress } from "../../../src/lib/types/ModuleAddress";
+import { parseGetValidImportUrlResultAsCouldConnect } from "./shared";
+
+describe("test 4", () => {
+    it("should get the valid for my_dummy_npm_and_deno_module from github repo", async () => {
+        const moduleAddress: ModuleAddress.GitHubRepo = {
+            "type": "GITHUB REPO",
+            "userOrOrg": "garronej",
+            "repositoryName": "my_dummy_npm_and_deno_module",
+            "branch": undefined
+        };
+
+        const getValidImportUrlFactoryResult = await getValidImportUrlFactory({
+            moduleAddress,
+            "desc": "MATCH VERSION INSTALLED IN NODE_MODULES",
+            "version": "0.4.3"
+        });
+
+        expect(getValidImportUrlFactoryResult.couldConnect).toBe(true);
+
+        const { versionFallbackWarning, getValidImportUrl } = parseGetValidImportUrlResultAsCouldConnect(getValidImportUrlFactoryResult);
+
+        expect(versionFallbackWarning).toBeUndefined();
+        expect(await getValidImportUrl({ "target": "DEFAULT EXPORT" })).toBe("https://deno.land/x/my_dummy_npm_and_deno_module@v0.4.3/mod.ts");
+        expect(await getValidImportUrl({ "target": "SPECIFIC FILE", "specificImportPath": "dist/lib/Cat" })).toBe(
+            "https://deno.land/x/my_dummy_npm_and_deno_module@v0.4.3/lib/Cat.ts"
+        );
+    });
+
+    it("should get the valid for leac from github repo", async () => {
+        const moduleAddress: ModuleAddress.GitHubRepo = {
+            "type": "GITHUB REPO",
+            "userOrOrg": "mxxii",
+            "repositoryName": "leac",
+            "branch": undefined
+        };
+        const getValidImportUrlFactoryResult = await getValidImportUrlFactory({
+            moduleAddress,
+            "desc": "MATCH VERSION INSTALLED IN NODE_MODULES",
+            "version": "0.6.0"
+        });
+
+        expect(getValidImportUrlFactoryResult.couldConnect).toBe(true);
+
+        const { versionFallbackWarning, getValidImportUrl } = parseGetValidImportUrlResultAsCouldConnect(getValidImportUrlFactoryResult);
+
+        expect(versionFallbackWarning).toBeUndefined();
+        expect(await getValidImportUrl({ "target": "DEFAULT EXPORT" })).toBe("https://deno.land/x/leac@v0.6.0/mod.ts");
+    });
+
+    it("should get the valid for my_dummy_npm_and_deno_module from github raw url", async () => {
+        const moduleAddress: ModuleAddress.GitHubRawUrl = {
+            "type": "GITHUB-RAW URL",
+            "baseUrlWithoutBranch": "https://raw.githubusercontent.com/garronej/my_dummy_npm_and_deno_module",
+            "branch": "v0.2.0",
+            "pathToIndex": "deno_dist/mod.ts"
+        };
+
+        expect(ModuleAddress.parse("https://raw.githubusercontent.com/garronej/my_dummy_npm_and_deno_module/v0.2.0/deno_dist/mod.ts")).toStrictEqual(
+            moduleAddress
+        );
+
+        expect(ModuleAddress.parse("https://raw.github.com/garronej/my_dummy_npm_and_deno_module/v0.2.0/deno_dist/mod.ts")).toStrictEqual(
+            moduleAddress
+        );
+
+        const getValidImportUrlFactoryResult = await getValidImportUrlFactory({
+            moduleAddress,
+            "desc": "MATCH VERSION INSTALLED IN NODE_MODULES",
+            "version": "v0.2.9"
+        });
+
+        expect(getValidImportUrlFactoryResult.couldConnect).toBe(true);
+
+        const { versionFallbackWarning, getValidImportUrl } = parseGetValidImportUrlResultAsCouldConnect(getValidImportUrlFactoryResult);
+
+        expect(versionFallbackWarning).toBeUndefined();
+        expect(await getValidImportUrl({ "target": "DEFAULT EXPORT" })).toBe(
+            "https://raw.githubusercontent.com/garronej/my_dummy_npm_and_deno_module/v0.2.9/deno_dist/mod.ts"
+        );
+    });
+});
